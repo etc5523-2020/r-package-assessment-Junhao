056e35e4b3789ea6e7a990a0a70685b6266c0313
foolnhungry <foolnhungry>
2020-10-24 00:03:02 +1100

    fix some technical issues

--- R/myfun.R
+++ R/myfun.R
@@ -7,16 +7,42 @@ library(tidyverse)
 
 
 
+#' Title
+#'
+#' @return
+#'
+#' @examples
+#' @export
 launch_app <- function(){shinyApp(ui = ui, server = server)}
 
 
+#' Title
+#'
+#' @return
+#'
+#' @examples
+#' @export
 uifunc <- function(){tabPanel("Table",fluid=TRUE,icon=icon("table"),
                               selectInput("typ","choose the type of COVID-19 cases",choices = auscov$type),
                               DT::dataTableOutput("TData"))}
 
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#'
+#' @examples
+#' @export
 selectthem<-function(x){selectInput(x, "choose one to inspect the daily confirmed cases in each state",
             choices = c("ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"))}
 
+#' Title
+#'
+#' @return
+#'
+#' @examples
+#' @export
 servfunc <- function(){data<-coronavirus%>%filter(country=="Australia")%>%mutate(
   state = case_when(
     province=="New South Wales" ~ "NSW",
--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of _your package name_ is to provide functions to simplify the codes in the shiny app.
+The goal of mypk is to provide functions to simplify the codes in the shiny app.
 
 ## Installation
 
-You can install the  _your package name_ with:
+You can install the  mypk with:
 
 ``` r
 install.packages("mypk")
